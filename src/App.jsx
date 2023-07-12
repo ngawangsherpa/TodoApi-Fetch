e8ca29d7dc59a7b8c05e476892b1d6e9270170db
Ngawang P Sherpa <ngawangpsherpa@gmail.com>
2023-07-12 02:45:59 -0400

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([]);
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+      const data = await res.json();
+      setTasks(data.slice(0, 5));
+    } catch (error) {
+      console.log("Error");
+    }
+  };
+
+  const addFromUrl = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/todos/6");
+      const newTask = await res.json();
+      setTasks([...tasks, newTask]);
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
+  const addTask = () => {
+    const newTask = {
+      id: tasks.length + 1,
+      title: newTaskTitle,
+      completed: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle("");
+  };
+
+  const removeTask = (id) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="con">
+      <div className="todo">
+        <h1>To-Do List</h1>
+        <div className="btn">
+          <button onClick={addFromUrl}>Add Task</button>
+        </div>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id} className="task">
+              {task.title}{" "}
+              <button onClick={() => removeTask(task.id)} className="rbtn">
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
